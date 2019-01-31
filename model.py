ab56b31449b1528b02eb60c5581ce3ad0df5cf3a
Fidel Serrano <serranoycandela@gmail.com>
2019-01-30 19:47:06 -0600

    Create model.py

---
+++ model.py
@@ -0,0 +1,13 @@
+matriz_c = [[++,+++,-...],
+            [++,+++,-...]
+            ..
+            ]
+
+v = Vector([.1,.3,.....], matriz_c)
+
+
+v.update(criterio = 1, delta = 3)
+
+v.hist()
+
+v.glyph()
